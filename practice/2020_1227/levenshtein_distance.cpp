199408fdf326ec290f85342794d8f382765dc453
takerumimata <fossil1908@gmail.com>
2020-12-27 15:35:15 +0900

    add levenshtein distance

---
+++ practice/2020_1227/levenshtein_distance.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+template<class T> void chmin(T& a, T b) {
+    if(a > b){
+        a = b;
+    }
+}
+
+const int INF = 1 <<29; // 十分大きな値（ここでは2^29とする）
+
+int main(void) {
+    // 入力
+    string S, T;
+    cin >> S >> T;
+
+    // DPテーブル定義
+    vector<vector<int>> dp(S.size() + 1, vector<int>(T.size() + 1, INF));
+
+    // DP初期条件
+    dp[0][0] = 0;
+
+    // DPループ
+    for(int i = 0; i <= S.size(); i++) {
+        for (int j = 0; j <= T.size(); j++) {
+            // 変更操作
+            if(i > 0 && j > 0) {
+                if(S[i - 1] == T[j-1]) {
+                    chmin(dp[i][j], dp[i - 1][j - 1]);
+                }
+                else {
+                    chmin(dp[i][j], dp[i - 1][j - 1] + 1);
+                }
+            }
+
+            // 削除操作
+            if(i > 0) chmin(dp[i][j], dp[i-1][j] +1);
+
+            // 挿入操作
+            if(j > 0) chmin(dp[i][j], dp[i][j-1] +1);
+        }
+        
+    }
+
+    // 答えの出力
+    cout << dp[S.size()][T.size()] << endl;
+    return 0;
+}
