b00fac64aada2cf53e0cdc9e60b78350b6965f02
takerumimata <fossil1908@gmail.com>
2021-04-18 14:53:27 +0900

    bug fix

--- leetcode/20210416/add-two-sum.cpp
+++ leetcode/20210416/add-two-sum.cpp
@@ -1,3 +1,5 @@
+#include<iostream>
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,6 +10,14 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
 
 class Solution {
 public:
