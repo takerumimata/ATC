93d4add12adf280e770408ed4ec138d66407d38b
takerumimata <fossil1908@gmail.com>
2021-07-18 19:03:16 +0900

    solve a

---
+++ practice/2021_0716/README.md
@@ -0,0 +1,2 @@
+# ABC004
+[問題](https://atcoder.jp/contests/abc004/tasks)
---
+++ practice/2021_0716/a.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(void){
+    int N;
+    cin >> N;
+    cout << 2 * N << endl;
+}
